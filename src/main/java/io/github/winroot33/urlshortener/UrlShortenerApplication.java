752a315faf7ece8c4a8b605c99fa785f9b068fe9
Mikhail Vasiliev <winroot123@gmail.com>
2026-05-17 15:31:01 +0300

    first commit

---
+++ src/main/java/io/github/winroot33/urlshortener/UrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package io.github.winroot33.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApplication.class, args);
+    }
+
+}
